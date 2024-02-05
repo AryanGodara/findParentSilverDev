c7f5fa40ed58efedea162261c5a13107e1fdf464
Aryan Godara <aryangodara03@gmail.com>
2024-02-05 21:35:01 +0530

    add testcases for softlinks

--- main_test.go
+++ main_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestFindParent(t *testing.T) {
-	// Create a more complex file tree.
 	root := newFile("root")
 	a := newFile("a")
 	b := newFile("b")
@@ -18,6 +17,18 @@ func TestFindParent(t *testing.T) {
 	f := newFile("f")
 	g := newFile("g")
 
+	// Add aliases.
+	x := newFile("x")
+	root.addAlias(x, d) // Alias "x" for d
+	y := newFile("y")
+	c.addAlias(y, f) // Alias "y" for f
+	z := newFile("z")
+	e.addAlias(z, a) // Alias "z" for a
+
+	// Adding more files for soft link demonstration
+	h := newFile("h") // New directory
+	i := newFile("i") // New file in 'h'
+
 	// Add children to form the tree.
 	root.addChild(a)
 	root.addChild(b)
@@ -26,14 +37,11 @@ func TestFindParent(t *testing.T) {
 	b.addChild(e)
 	d.addChild(f)
 	c.addChild(g)
+	h.addChild(i) // Add 'i' to 'h'
 
-	// Add aliases.
-	x := newFile("x")
-	root.addAlias(x, d) // Alias "x" for d
-	y := newFile("y")
-	c.addAlias(y, f) // Alias "y" for f
-	z := newFile("z")
-	e.addAlias(z, a) // Alias "z" for a
+	// Incorporating soft links (previously aliases).
+	root.addAlias(h, d) // Soft link "h" to "d", simulating a directory 'h' pointing to 'd'
+	c.addAlias(i, f)    // Soft link "i" to "f", simulating a file 'i' pointing to 'f'
 
 	// Test cases including nil files, and aliases.
 	cases := []struct {
@@ -61,14 +69,6 @@ func TestFindParent(t *testing.T) {
 			err:    nil,
 		},
 		{
-			desc:   "Alias x pointing to d",
-			root:   root,
-			file1:  x,
-			file2:  f,
-			parent: d,
-			err:    nil,
-		},
-		{
 			desc:   "Nil file1",
 			root:   root,
 			file1:  nil,
@@ -85,6 +85,14 @@ func TestFindParent(t *testing.T) {
 			err:    errFileNil,
 		},
 		{
+			desc:   "Alias x pointing to d",
+			root:   root,
+			file1:  x,
+			file2:  f,
+			parent: d,
+			err:    nil,
+		},
+		{
 			desc:   "Alias y and g",
 			root:   root,
 			file1:  y,
@@ -98,6 +106,30 @@ func TestFindParent(t *testing.T) {
 			file1:  z,
 			file2:  b,
 			parent: root,
+			err:    nil,
+		},
+		{
+			desc:   "Common parent of c and d, considering soft links",
+			root:   root,
+			file1:  c,
+			file2:  d,
+			parent: a,
+			err:    nil,
+		},
+		{
+			desc:   "Soft link 'h' pointing to 'd' and its child 'f'",
+			root:   root,
+			file1:  h, // Now represents a soft link to 'd'
+			file2:  a,
+			parent: a, // Expected common parent is 'a', since 'h' is a soft link to 'd'
+			err:    nil,
+		},
+		{
+			desc:   "Soft link 'i' within 'h' pointing to 'f'",
+			root:   root,
+			file1:  i, // Represents a soft link/file in 'h' pointing to 'f'
+			file2:  g,
+			parent: a, // 'g' is under 'c', and 'i' is a soft link pointing to 'f' under 'd', both under 'a'
 			err:    nil,
 		},
 	}
